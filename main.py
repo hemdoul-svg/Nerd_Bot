f6cbe8ce48b8e51717c2d00ac7850a6ff3cdd705
hemdoul-svg <hemdoul@yahoo.com>
2026-05-06 10:28:48 +0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import json
 import os
 from dotenv import load_dotenv # ça sert à garder le Token en .env .
 load_dotenv ()
-token= os.getenv('Discord_TOKEN')
+token= os.getenv('TOKEN')
 
 # ==============================
 # CONFIGURATION DU BOT
