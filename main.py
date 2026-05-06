2b0d3419cea56d3e371502b8bc0c896b9b75a905
hemdoul-svg <hemdoul@yahoo.com>
2026-05-06 12:03:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import discord
 from discord import app_commands
 import random
 import datetime
-import json
 import os
 from dotenv import load_dotenv # ça sert à garder le Token en .env .
 load_dotenv ()
 TOKEN= os.getenv("TOKEN")
 
+if TOKEN is None:
+    raise Exception("TOKEN manquant")
 # ==============================
 # CONFIGURATION DU BOT
 # ==============================
@@ -105,10 +106,74 @@ async def help(interaction: discord.Interaction):
 
     await interaction.response.send_message(message)
 
+#===============================
+# Commande: /clash
+#===============================
+
+@tree.command(name="clash", description="Clash nerd entre 2 joueurs ⚔️")
+async def clash(
+    interaction: discord.Interaction,
+    opponent: discord.Member,
+    phrase1: str,
+    phrase2: str
+):
+
+    def style(text):
+        prefixes = ["DEBUG:", "AI SAYS:", "KERNEL:"]
+        suffixes = ["💀", "🤖", "⚡"]
+        return f"{random.choice(prefixes)} {text} {random.choice(suffixes)}"
+
+    c1 = style(phrase1)
+    c2 = style(phrase2)
+
+    score1 = calculate_score(phrase1)
+    score2 = calculate_score(phrase2)
+
+    if score1 > score2:
+        winner = interaction.user
+    elif score2 > score1:
+        winner = opponent
+    else:
+        winner = None
+
+    result = "🤝 ÉGALITÉ !" if winner is None else f"🏆 Gagnant : {winner.mention}"
+
+    await interaction.response.send_message(
+        f"⚔️ **CLASH NERD DUEL** ⚔️\n\n"
+        f"👤 {interaction.user.mention} : {c1} — {score1} pts\n"
+        f"👤 {opponent.mention} : {c2} — {score2} pts\n\n"
+        f"{result}"
+    )
+
+
+
+#===============================
+# SYSTEME DE SCORING
+#===============================
+ 
+nerd_words = [
+    "python", "bug", "cpu", "ram", "code", "debug",
+    "linux", "algorithm", "server", "gpu", "compile","windows","ordinateur","gpu"
+]
+
+def calculate_score(text):
+    text = text.lower()
+
+    score = 20  # base
+
+    for word in nerd_words:
+        if word in text:
+            score += 10
+
+    score += min(len(text) // 5, 20)
+    score += random.randint(0, 30)
+
+    return score
+
+
 # ==============================
 # LANCEMENT DU BOT
 # ==============================
 
 
 client.run(TOKEN)
-
